b17b69c86cd227b80488184745f4ceb0e8634421
Rassul <rasul.tabulov@gmail.com>
2021-07-19 23:35:37 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container text-center py-5 bg-light">
+    <h1 class="display-1 mb-5">My personal website!!!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum rem
+      impedit dolorum odit ipsum iure?
+    </p>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss"></style>
