dbe1054b7ade5edff1e06ff19654c8ac26e0a10a
Rassul <rasul.tabulov@gmail.com>
2021-07-22 22:55:31 +0600

    Updates

---
+++ assets/Cinemas.42a4a426.js
@@ -0,0 +1,1 @@
+import{c as e,o as n}from"./vendor.e27b753a.js";const r={};r.render=function(r,o){return n(),e("h1",null,"Cinemas")};export default r;
---
+++ assets/Tickets.a1b9169e.js
@@ -0,0 +1,1 @@
+import{c as e,o as r}from"./vendor.e27b753a.js";const n={};n.render=function(n,o){return r(),e("h1",null,"Tickets")};export default n;
