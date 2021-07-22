a3d9aa9352cc6e2397f064d75ff8e5c4242c40f8
Rassul <rasul.tabulov@gmail.com>
2021-07-22 23:05:38 +0600

    production fixes

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,11 @@ const purgecss = require('@fullhuman/postcss-purgecss');
 module.exports = {
   plugins: [
     purgecss({
-      content: ['./src/**/*.vue'],
-      safelist: [/.*(swiper).*/, 'html', '*', 'body'],
+      content: ['./src/**/*.vue', './index.html'],
+      safelist: [
+        /.*(swiper).*/, // swiper.js
+        /.*(tab).*/, // tabs-slider.js
+      ],
     }),
   ],
 };
